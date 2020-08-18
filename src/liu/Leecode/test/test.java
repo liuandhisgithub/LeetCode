09295f0a51d89b38b102c36e00d948aa2c395c61
Your Name <you@example.com>
2020-08-18 22:06:18 +0800

    面向答案ac大法果然威力无穷，睡了睡了

--- src/liu/Leecode/test/test.java
+++ src/liu/Leecode/test/test.java
@@ -3,11 +3,13 @@ package liu.Leecode.test;
 
 import liu.Leecode.thirtyOneToFourTy.Solution;
 
+import java.util.Arrays;
+
 public class test {
     public static void main(String[] args){
         Solution solution = new Solution();
-        int[] nums = new int[]{1,2,3};
-        solution.nextPermutation(nums);
+        int[] nums = new int[]{5,1,1};
+        Arrays.sort(nums);
         System.out.println(nums[0]);
     }
 }
--- src/liu/Leecode/thirtyOneToFourTy/Solution.java
+++ src/liu/Leecode/thirtyOneToFourTy/Solution.java
@@ -8,23 +8,28 @@ public class Solution {
      * @param nums
      */
     public void nextPermutation(int[] nums) {
-        int index = nums.length - 1;
-        while(index > 0 && nums[index] < nums[index-1]){
-            index -- ;
+        int index = nums.length - 2;
+        while(index >= 0 && nums[index] >= nums[index + 1]){
+            index--;
         }
-        if(index == 0) {
-            Arrays.sort(nums);
-            return ;
+        if(index>=0){
+            int j = index + 1;
+            while(j<nums.length&&nums[j]>nums[index]){
+                j++;
+            }
+            j--;
+            int temp = nums[index];
+            nums[index] = nums[j];
+            nums[j] = temp;
         }
-        index --;
-        int index2 = index+1;
-        while(index2<nums.length&&nums[index]<nums[index2]){
-            index2++;
+        for(int i = index + 1;i<nums.length;i++){
+            for(int j = i + 1; j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
         }
-        index2--;
-        int temp = nums[index2];
-        nums[index2] = nums[index];
-        nums[index] = temp;
-        return;
     }
 }
