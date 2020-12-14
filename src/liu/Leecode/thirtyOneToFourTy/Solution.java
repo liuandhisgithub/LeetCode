aa0ec475f3996b20ff5f015fb3e76c7df0476f15
liuxinlei <liujinlei2016@163.com>
2020-12-14 17:06:52 +0800

    A了一题，好久没A题了

--- src/liu/Leecode/thirtyOneToFourTy/Solution.java
+++ src/liu/Leecode/thirtyOneToFourTy/Solution.java
@@ -22,6 +22,7 @@ public class Solution {
             nums[index] = nums[j];
             nums[j] = temp;
         }
+        //排序 交换排序
         for(int i = index + 1;i<nums.length;i++){
             for(int j = i + 1; j<nums.length;j++){
                 if(nums[i]>nums[j]){
@@ -32,4 +33,42 @@ public class Solution {
             }
         }
     }
+    /**
+     * 34 在排序数组中查找元素的第一个和最后一个位置
+     */
+    public int[] searchRange(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start < end){
+            int mid = (end + start) / 2;
+            if(nums[mid] == target){
+                start = mid;
+                end = mid;
+                break;
+            } else if(nums[mid] > target){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        if(start >= nums.length || nums[start] != target ) {
+            return new int[]{-1,-1};
+        }
+        else {
+            while(start >= 0 && nums[start] == target){
+                start --;
+            }
+            while (end < nums.length && nums[end] == target){
+                end ++;
+            }
+            return new int[]{start+1, end-1};
+        }
+    }
+    /**
+     * 35. 搜索插入位置
+     */
+    public int searchInsert(int[] nums, int target) {
+        return 0;
+    }
+
 }
